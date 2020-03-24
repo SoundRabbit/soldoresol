0c7a3c1621fd44f9b249410a8587314e545da990
SoundRabbit <soundrabbitstd@gmail.com>
2020-03-24 23:17:29 +0900

    メインメニュー

--- src/component/app.rs
+++ src/component/app.rs
@@ -27,5 +27,44 @@ fn render(_: &State) -> Html<Msg> {
 }
 
 fn render_side_menu() -> Html<Msg> {
-    Html::menu(Attributes::new().id("app-side-menu"), Events::new(), vec![])
+    Html::div(
+        Attributes::new().id("app-side-menu"),
+        Events::new(),
+        vec![
+            render_side_menu_item("テーブル"),
+            render_side_menu_item("画像"),
+            render_side_menu_item("音楽"),
+            render_side_menu_item("キャラクター"),
+            render_side_menu_item("オブジェクト"),
+            render_side_menu_item("資料"),
+            render_side_menu_item("チャット"),
+        ],
+    )
+}
+
+fn render_side_menu_item(text: impl Into<String>) -> Html<Msg> {
+    Html::div(
+        Attributes::new().class("app-side-menu-item"),
+        Events::new(),
+        vec![
+            Html::div(
+                Attributes::new().class("app-side-menu-item-content-wrapper"),
+                Events::new(),
+                vec![Html::div(
+                    Attributes::new().class("app-side-menu-item-text"),
+                    Events::new(),
+                    vec![Html::text(text)],
+                )],
+            ),
+            Html::div(
+                Attributes::new().class("app-side-menu-item-content-wrapper"),
+                Events::new(),
+                vec![Html::div(
+                    Attributes::new().class("app-side-menu-item-frame"),
+                    Events::new(),
+                    vec![],
+                )],
+            ),
+        ],
+    )
 }
