702a8ad750c349000619101f96bf522bb8810160
SoundRabbit <soundrabbitstd@gmail.com>
2020-07-02 19:16:04 +0900

    side_menuに分離

--- src/component/room/render/mod.rs
+++ src/component/room/render/mod.rs
@@ -1,14 +1,10 @@
-use super::super::{awesome, btn, color_picker, dropdown, text};
 use super::{
-    state::{self, table, Modal, Modeless},
+    state::{self, Modal},
     Msg, State,
 };
-use crate::{
-    block::{self, BlockId},
-    color_system,
-};
+use crate::block::{self};
 use kagura::prelude::*;
-use wasm_bindgen::{prelude::*, JsCast};
+use wasm_bindgen::JsCast;
 
 mod canvas_container;
 mod common;
@@ -16,6 +12,7 @@ mod contextmenu;
 mod header_menu;
 mod modal;
 mod modeless;
+mod side_menu;
 
 pub fn render(state: &State) -> Html<Msg> {
     Html::div(
@@ -46,7 +43,7 @@ pub fn render(state: &State) -> Html<Msg> {
                 state.table().selecting_tool(),
                 state.table().is_2d_mode(),
             ),
-            side_menu(state.table().selecting_tool()),
+            side_menu::render(state.table().selecting_tool()),
             if let Some(world) = state.block_field().get::<block::World>(state.world()) {
                 canvas_container::render(&state, world)
             } else {
@@ -60,217 +57,4 @@ pub fn render(state: &State) -> Html<Msg> {
             modal::render(state.block_field(), state.resource(), state.modal(), state),
         ],
     )
-}
-
-fn side_menu(selecting_tool: &table::Tool) -> Html<Msg> {
-    Html::div(
-        Attributes::new()
-            .class("panel")
-            .class("keyvalue")
-            .class("keyvalue-rev")
-            .style("overflow", "visible")
-            .style("z-index", "1"),
-        Events::new(),
-        vec![
-            row(
-                selecting_tool.is_selector(),
-                "fa-mouse-pointer",
-                "選択",
-                Events::new().on_click(|_| Msg::SetSelectingTableTool(table::Tool::Selector)),
-                match selecting_tool {
-                    table::Tool::Selector => option(false, Events::new(), vec![]),
-                    _ => text::span(""),
-                },
-            ),
-            delm("描画"),
-            row(
-                selecting_tool.is_pen(),
-                "fa-pen",
-                "ペン",
-                Events::new().on_click(|_| {
-                    Msg::SetSelectingTableTool(table::Tool::Pen {
-                        line_width: 0.5,
-                        color: color_system::gray(255, 9),
-                        show_option_menu: false,
-                    })
-                }),
-                match selecting_tool {
-                    table::Tool::Pen {
-                        line_width,
-                        color,
-                        show_option_menu,
-                    } => {
-                        let line_width = *line_width;
-                        let color = *color;
-                        let show_option_menu = *show_option_menu;
-                        option(
-                            show_option_menu,
-                            Events::new().on_click(move |_| {
-                                Msg::SetSelectingTableTool(table::Tool::Pen {
-                                    line_width: line_width,
-                                    color: color,
-                                    show_option_menu: !show_option_menu,
-                                })
-                            }),
-                            vec![
-                                Html::div(
-                                    Attributes::new().class("keyvalue"),
-                                    Events::new(),
-                                    vec![
-                                        text::span("太さ"),
-                                        Html::input(
-                                            Attributes::new()
-                                                .type_("number")
-                                                .value(line_width.to_string())
-                                                .string("step", "0.1"),
-                                            Events::new().on_input(move |w| {
-                                                w.parse()
-                                                    .map(|w| {
-                                                        Msg::SetSelectingTableTool(
-                                                            table::Tool::Pen {
-                                                                line_width: w,
-                                                                color: color,
-                                                                show_option_menu: show_option_menu,
-                                                            },
-                                                        )
-                                                    })
-                                                    .unwrap_or(Msg::NoOp)
-                                            }),
-                                            vec![],
-                                        ),
-                                    ],
-                                ),
-                                color_picker::major(Msg::NoOp, move |color| {
-                                    Msg::SetSelectingTableTool(table::Tool::Pen {
-                                        line_width: line_width,
-                                        color: color,
-                                        show_option_menu: show_option_menu,
-                                    })
-                                }),
-                            ],
-                        )
-                    }
-                    _ => text::span(""),
-                },
-            ),
-            row(
-                selecting_tool.is_eracer(),
-                "fa-eraser",
-                "消しゴム",
-                Events::new().on_click(|_| {
-                    Msg::SetSelectingTableTool(table::Tool::Eracer {
-                        show_option_menu: false,
-                    })
-                }),
-                match selecting_tool {
-                    table::Tool::Eracer { show_option_menu } => {
-                        let show_option_menu = *show_option_menu;
-                        option(
-                            show_option_menu,
-                            Events::new().on_click(move |_| {
-                                Msg::SetSelectingTableTool(table::Tool::Eracer {
-                                    show_option_menu: !show_option_menu,
-                                })
-                            }),
-                            vec![],
-                        )
-                    }
-                    _ => text::span(""),
-                },
-            ),
-            delm("作成"),
-            row(
-                selecting_tool.is_area(),
-                "fa-ruler-combined",
-                "範囲",
-                Events::new().on_click(|_| {
-                    Msg::SetSelectingTableTool(table::Tool::Area {
-                        line_width: 2.0,
-                        is_rounded: false,
-                    })
-                }),
-                match selecting_tool {
-                    table::Tool::Area { .. } => option(false, Events::new(), vec![]),
-                    _ => text::span(""),
-                },
-            ),
-            row(
-                selecting_tool.is_route(),
-                "fa-route",
-                "経路",
-                Events::new().on_click(|_| Msg::SetSelectingTableTool(table::Tool::Route(None))),
-                match selecting_tool {
-                    table::Tool::Route(..) => option(false, Events::new(), vec![]),
-                    _ => text::span(""),
-                },
-            ),
-            delm("表示"),
-            row(
-                selecting_tool.is_measure(),
-                "fa-ruler",
-                "距離",
-                Events::new().on_click(|_| Msg::SetSelectingTableTool(table::Tool::Measure(None))),
-                match selecting_tool {
-                    table::Tool::Measure(..) => option(false, Events::new(), vec![]),
-                    _ => text::span(""),
-                },
-            ),
-        ]
-        .into_iter()
-        .flatten()
-        .collect(),
-    )
-}
-
-fn row(
-    selected: bool,
-    icon: impl Into<String>,
-    text: impl Into<String>,
-    btn_event: Events<Msg>,
-    option: Html<Msg>,
-) -> Vec<Html<Msg>> {
-    vec![
-        btn::selectable(
-            selected,
-            Attributes::new(),
-            btn_event,
-            vec![awesome::i(icon), Html::text(" "), text::span(text)],
-        ),
-        option,
-    ]
-}
-
-fn option(show_option: bool, events: Events<Msg>, menu: Vec<Html<Msg>>) -> Html<Msg> {
-    dropdown::right_bottom(
-        show_option,
-        btn::transparent(
-            Attributes::new(),
-            events,
-            vec![awesome::i("fa-angle-right")],
-        ),
-        Html::div(
-            Attributes::new()
-                .class("panel")
-                .class("pure-form")
-                .class("linear-v")
-                .style("width", "max-content"),
-            Events::new(),
-            menu,
-        ),
-    )
-}
-
-fn delm(text: impl Into<String>) -> Vec<Html<Msg>> {
-    vec![
-        Html::hr(
-            Attributes::new().class("keyvalue-banner"),
-            Events::new(),
-            vec![],
-        ),
-        Html::span(
-            Attributes::new().class("keyvalue-banner"),
-            Events::new(),
-            vec![Html::text(text)],
-        ),
-    ]
 }
---
+++ src/component/room/render/side_menu.rs
@@ -0,0 +1,217 @@
+use super::super::super::{awesome, btn, color_picker, dropdown, text};
+use super::{state::table, Msg};
+use crate::color_system;
+use kagura::prelude::*;
+
+pub fn render(selecting_tool: &table::Tool) -> Html<Msg> {
+    Html::div(
+        Attributes::new()
+            .class("panel")
+            .class("keyvalue")
+            .class("keyvalue-rev")
+            .style("overflow", "visible")
+            .style("z-index", "1"),
+        Events::new(),
+        vec![
+            row(
+                selecting_tool.is_selector(),
+                "fa-mouse-pointer",
+                "選択",
+                Events::new().on_click(|_| Msg::SetSelectingTableTool(table::Tool::Selector)),
+                match selecting_tool {
+                    table::Tool::Selector => option(false, Events::new(), vec![]),
+                    _ => text::span(""),
+                },
+            ),
+            delm("描画"),
+            row(
+                selecting_tool.is_pen(),
+                "fa-pen",
+                "ペン",
+                Events::new().on_click(|_| {
+                    Msg::SetSelectingTableTool(table::Tool::Pen {
+                        line_width: 0.5,
+                        color: color_system::gray(255, 9),
+                        show_option_menu: false,
+                    })
+                }),
+                match selecting_tool {
+                    table::Tool::Pen {
+                        line_width,
+                        color,
+                        show_option_menu,
+                    } => {
+                        let line_width = *line_width;
+                        let color = *color;
+                        let show_option_menu = *show_option_menu;
+                        option(
+                            show_option_menu,
+                            Events::new().on_click(move |_| {
+                                Msg::SetSelectingTableTool(table::Tool::Pen {
+                                    line_width: line_width,
+                                    color: color,
+                                    show_option_menu: !show_option_menu,
+                                })
+                            }),
+                            vec![
+                                Html::div(
+                                    Attributes::new().class("keyvalue"),
+                                    Events::new(),
+                                    vec![
+                                        text::span("太さ"),
+                                        Html::input(
+                                            Attributes::new()
+                                                .type_("number")
+                                                .value(line_width.to_string())
+                                                .string("step", "0.1"),
+                                            Events::new().on_input(move |w| {
+                                                w.parse()
+                                                    .map(|w| {
+                                                        Msg::SetSelectingTableTool(
+                                                            table::Tool::Pen {
+                                                                line_width: w,
+                                                                color: color,
+                                                                show_option_menu: show_option_menu,
+                                                            },
+                                                        )
+                                                    })
+                                                    .unwrap_or(Msg::NoOp)
+                                            }),
+                                            vec![],
+                                        ),
+                                    ],
+                                ),
+                                color_picker::major(Msg::NoOp, move |color| {
+                                    Msg::SetSelectingTableTool(table::Tool::Pen {
+                                        line_width: line_width,
+                                        color: color,
+                                        show_option_menu: show_option_menu,
+                                    })
+                                }),
+                            ],
+                        )
+                    }
+                    _ => text::span(""),
+                },
+            ),
+            row(
+                selecting_tool.is_eracer(),
+                "fa-eraser",
+                "消しゴム",
+                Events::new().on_click(|_| {
+                    Msg::SetSelectingTableTool(table::Tool::Eracer {
+                        show_option_menu: false,
+                    })
+                }),
+                match selecting_tool {
+                    table::Tool::Eracer { show_option_menu } => {
+                        let show_option_menu = *show_option_menu;
+                        option(
+                            show_option_menu,
+                            Events::new().on_click(move |_| {
+                                Msg::SetSelectingTableTool(table::Tool::Eracer {
+                                    show_option_menu: !show_option_menu,
+                                })
+                            }),
+                            vec![],
+                        )
+                    }
+                    _ => text::span(""),
+                },
+            ),
+            delm("作成"),
+            row(
+                selecting_tool.is_area(),
+                "fa-ruler-combined",
+                "範囲",
+                Events::new().on_click(|_| {
+                    Msg::SetSelectingTableTool(table::Tool::Area {
+                        line_width: 2.0,
+                        is_rounded: false,
+                    })
+                }),
+                match selecting_tool {
+                    table::Tool::Area { .. } => option(false, Events::new(), vec![]),
+                    _ => text::span(""),
+                },
+            ),
+            row(
+                selecting_tool.is_route(),
+                "fa-route",
+                "経路",
+                Events::new().on_click(|_| Msg::SetSelectingTableTool(table::Tool::Route(None))),
+                match selecting_tool {
+                    table::Tool::Route(..) => option(false, Events::new(), vec![]),
+                    _ => text::span(""),
+                },
+            ),
+            delm("表示"),
+            row(
+                selecting_tool.is_measure(),
+                "fa-ruler",
+                "距離",
+                Events::new().on_click(|_| Msg::SetSelectingTableTool(table::Tool::Measure(None))),
+                match selecting_tool {
+                    table::Tool::Measure(..) => option(false, Events::new(), vec![]),
+                    _ => text::span(""),
+                },
+            ),
+        ]
+        .into_iter()
+        .flatten()
+        .collect(),
+    )
+}
+
+fn row(
+    selected: bool,
+    icon: impl Into<String>,
+    text: impl Into<String>,
+    btn_event: Events<Msg>,
+    option: Html<Msg>,
+) -> Vec<Html<Msg>> {
+    vec![
+        btn::selectable(
+            selected,
+            Attributes::new(),
+            btn_event,
+            vec![awesome::i(icon), Html::text(" "), text::span(text)],
+        ),
+        option,
+    ]
+}
+
+fn option(show_option: bool, events: Events<Msg>, menu: Vec<Html<Msg>>) -> Html<Msg> {
+    dropdown::right_bottom(
+        show_option,
+        btn::transparent(
+            Attributes::new(),
+            events,
+            vec![awesome::i("fa-angle-right")],
+        ),
+        Html::div(
+            Attributes::new()
+                .class("panel")
+                .class("pure-form")
+                .class("linear-v")
+                .style("width", "max-content"),
+            Events::new(),
+            menu,
+        ),
+    )
+}
+
+fn delm(text: impl Into<String>) -> Vec<Html<Msg>> {
+    vec![
+        Html::hr(
+            Attributes::new().class("keyvalue-banner"),
+            Events::new(),
+            vec![],
+        ),
+        Html::span(
+            Attributes::new().class("keyvalue-banner"),
+            Events::new(),
+            vec![Html::text(text)],
+        ),
+    ]
+}
